a0a844dc575472ee988735b92e3b60657ed2eab5
Khushi Kadyan <kadyankhushi290@gmail.com>
2025-06-06 14:53:20 +0530

    Add solution for airthmatic operation

---
+++ Airthmatic_Operator.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    print("Enter the two Numbers for arithmatic operations: ")
+    a = int(input())
+    b = int(input())
+    
+    print("Addition- ")
+    print(a+b)
+   
+    print("Subtration results- ")
+    print(a-b)
+
+    print("Multiplication results- ")
+    print(a*b)
+
+    
