74145297a111f16df63296df9ef39038ef480893
Khushi Kadyan <143698815+khushikadyan@users.noreply.github.com>
2025-06-11 20:14:41 +0530

    Delete Compress_string.py

--- Compress_string.py
+++
@@ -1,8 +0,0 @@
-from itertools import groupby
-print("Enter the string")
-s = input().strip()
-print("Result:")
-for key, group in groupby(s):
-    count = len(list(group))      
-    number = int(key)             
-    print((count, number), end=' ')
